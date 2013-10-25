82fd8df4c34e7f4508647aaeacbe3d97532a3676
Thomas Apicella <tapicella.ms@gmail.com>
2013-10-25 17:15:02 -0400

    Bump version

--- metridoc-grails-rid/MetridocRidGrailsPlugin.groovy
+++ metridoc-grails-rid/MetridocRidGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class MetridocRidGrailsPlugin {
     // the plugin version
-    def version = "0.3.3"
+    def version = "0.7.11-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
