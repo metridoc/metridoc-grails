c64a6f5b6cab36f1c1954a9b6fc1c28a4186a645
tbarker <mingus.karate@gmail.com>
2013-11-14 15:13:01 -0500

    extract ldap operations
    all ldap operations have been extracted into a separate service.  It
    works according to old functionality

--- metridoc-grails-core/grails-app/services/metridoc/core/LdapOperationsService.groovy
+++ metridoc-grails-core/grails-app/services/metridoc/core/LdapOperationsService.groovy
@@ -1,24 +1,86 @@
 package metridoc.core
 
+import org.apache.shiro.authc.IncorrectCredentialsException
+import org.apache.shiro.authc.UnknownAccountException
+
+import javax.naming.AuthenticationException
 import javax.naming.Context
 import javax.naming.directory.InitialDirContext
+import javax.naming.directory.SearchControls
 
 @SuppressWarnings("GroovyAssignabilityCheck")
 class LdapOperationsService {
 
     InitialDirContext connect() {
-        new InitialDirContext(getEnvFromLdapData())
+        new InitialDirContext(getEnvFromLdapData() as Hashtable)
     }
 
-    HashMap getEnvFromLdapData() {
+    Hashtable getEnvFromLdapData() {
+        LdapData ldapData = LdapData.first()
+
+        return getEnvFromLdapData(ldapData.managerDN, ldapData.unencryptedPassword)
+    }
+
+    Hashtable getEnvFromLdapData(String userDN, String unencryptedPassword) {
         LdapData ldapData = LdapData.first()
         def env = new Hashtable()
 
         env[Context.INITIAL_CONTEXT_FACTORY] = "com.sun.jndi.ldap.LdapCtxFactory"
-        env[Context.SECURITY_PRINCIPAL] = ldapData.managerDN
-        env[Context.SECURITY_CREDENTIALS] = ldapData.unencryptedPassword
+        if (userDN != null) {
+            env[Context.SECURITY_PRINCIPAL] = userDN
+            if (unencryptedPassword != null) {
+                env[Context.SECURITY_CREDENTIALS] = unencryptedPassword
+            }
+        }
         env[Context.PROVIDER_URL] = ldapData.server
 
         return env
+    }
+
+    String getUserQuery(String username) {
+        getUserQuery(username, LdapData.first().userSearchFilter)
+    }
+
+    void validateUserCredentials(String userName, String unencryptedPassword) {
+        try {
+            def env = getEnvFromLdapData(userName, unencryptedPassword)
+            new InitialDirContext(env as Hashtable)
+        }
+        catch (AuthenticationException ignored) {
+            log.info "Invalid password for [$userName]"
+            throw new IncorrectCredentialsException("Invalid password for user '${userName}'")
+        }
+    }
+
+    String validateThatUserExists(String userName, InitialDirContext initialDirContext) {
+        LdapData data = LdapData.first()
+        validateThatUserExists(userName, getUserQuery(userName), data.userSearchBase, data.rootDN, initialDirContext)
+    }
+
+    protected static String validateThatUserExists(String username, String filter, String userSearchBase,
+                                                   String rootDN, InitialDirContext initialDirContext) throws UnknownAccountException {
+
+        SearchControls searchControls = new SearchControls()
+
+        searchControls.setSearchScope(2)
+
+        def base = "$userSearchBase,$rootDN"
+        def result = initialDirContext.search(base, filter, searchControls)
+        if (!result.hasMore()) {
+            throw new UnknownAccountException("No account found for user [${username}]")
+        }
+
+        result.next().nameInNamespace
+    }
+
+    protected static String getUserQuery(String username, String filter) {
+        String result
+        if (filter.contains("{0}")) {
+            result = "${filter.replaceAll(/\{0\}/, username)}"
+        }
+        else {
+            result = "$filter=$username"
+        }
+        return "($result)"
     }
 }
--- metridoc-grails-core/test/unit/metridoc/core/LdapOperationsServiceSpec.groovy
+++ metridoc-grails-core/test/unit/metridoc/core/LdapOperationsServiceSpec.groovy
@@ -2,13 +2,16 @@ package metridoc.core
 
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
+import org.apache.shiro.authc.UnknownAccountException
 import spock.lang.Specification
 
 import javax.naming.Context
+import javax.naming.NamingEnumeration
+import javax.naming.NamingException
+import javax.naming.directory.InitialDirContext
+import javax.naming.directory.SearchControls
+import javax.naming.directory.SearchResult
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(LdapOperationsService)
 @Mock([LdapData, CryptKey])
 class LdapOperationsServiceSpec extends Specification {
@@ -17,10 +20,7 @@ class LdapOperationsServiceSpec extends Specification {
         new CryptKey(
                 encryptKey: "123123"
         ).save(failOnError: true)
-    }
 
-    void "test extracting environment from LdapData"() {
-        given:
         def encryptionService = new EncryptionService()
         def ldapData = new LdapData(
                 server: "ldap://foo.bar",
@@ -31,8 +31,11 @@ class LdapOperationsServiceSpec extends Specification {
         )
         encryptionService.encryptLdapData(ldapData, "password")
         ldapData.save(failOnError: true)
+    }
 
+    void "test extracting environment from LdapData"() {
         when:
+        def ldapData = LdapData.first()
         Hashtable env = service.getEnvFromLdapData()
 
         then:
@@ -40,5 +43,93 @@ class LdapOperationsServiceSpec extends Specification {
         env[Context.SECURITY_PRINCIPAL] == ldapData.managerDN
         env[Context.SECURITY_CREDENTIALS] == ldapData.unencryptedPassword
         env[Context.PROVIDER_URL] == ldapData.server
+
+        when: "user name is null"
+        env = service.getEnvFromLdapData(null, null)
+
+        then:
+        2 == env.size()
+        !env.containsKey(Context.SECURITY_PRINCIPAL)
+        !env.containsKey(Context.SECURITY_CREDENTIALS)
+
+        when: "password is null"
+        env = service.getEnvFromLdapData("foo", null)
+
+        then:
+        3 == env.size()
+        !env.containsKey(Context.SECURITY_CREDENTIALS)
+    }
+
+    void "test generating user filter"() {
+        expect:
+        a == service.getUserQuery(b, c)
+
+        where:
+        a              || b | c
+        "(bar=foo)"    || "foo" | "bar"
+        "(foobar=foo)" || "foo" | "foobar={0}"
+    }
+
+    void "test generating data from stored filter"() {
+        expect:
+        a == service.getUserQuery(b)
+
+        where:
+        a              || b
+        "(foobar=bar)" || "bar"
+    }
+
+    void "test validating a user existence"() {
+        when:
+        def result = service.validateThatUserExists("foo", "bar", "foo", "foobar", new InitialDirContextMock())
+
+        then:
+        noExceptionThrown()
+        "foobar" == result
+
+        when:
+        service.validateThatUserExists("base", "bar", "baz", "foobar", new InitialDirContextMock())
+
+        then:
+        thrown(UnknownAccountException)
+
+        when:
+        result = service.validateThatUserExists("foo", new InitialDirContextMock())
+
+        then:
+        noExceptionThrown()
+        "foobar" == result
+    }
+}
+
+class InitialDirContextMock extends InitialDirContext {
+    @Override
+    NamingEnumeration<SearchResult> search(String name, String filter, SearchControls cons) throws NamingException {
+        def trueNamingEnumeration = [
+                hasMore: {
+                    true
+                },
+                next: {
+                    def result = new SearchResult(null, null, null)
+                    result.nameInNamespace = "foobar"
+
+                    return result
+                }
+        ] as NamingEnumeration
+
+        if ("foo,foobar" == name) {
+            return trueNamingEnumeration
+        }
+
+        def ldapData = LdapData.first()
+        if("${ldapData.userSearchBase},${ldapData.rootDN}" == name) {
+            return trueNamingEnumeration
+        }
+
+        [
+                hasMore: {
+                    false
+                }
+        ] as NamingEnumeration
     }
 }
