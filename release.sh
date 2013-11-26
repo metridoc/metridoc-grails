39d9d412068a53a60c2eeb77f6c6b5e1ec30b995
tbarker <mingus.karate@gmail.com>
2013-11-26 10:01:07 -0500

    improving build

--- release.sh
+++ release.sh
@@ -2,42 +2,11 @@
 
 source helper.sh
 
-systemCall() {
-    echo "running $1"
-    if eval $1; then
-		echo "command [$1] ran successfully"
-	else
-		echo "command [$1] failed with exit status [$?]"
-		exit 1
-	fi
-}
-
-DIRECTORIES=`find ./metridoc-grails* -type d -maxdepth 0 -mindepth 0`
-
-VERSION=`cat VERSION`
 if grep -q "\-SNAPSHOT" "VERSION"; then
     echo "VERSION file has SNAPSHOT in it, skipping release and just running build"
     source buildAll.sh
     exit 0
 fi
-
-synchronizeVersions() {
-
-    echo ""
-    echo "synchronizing all versions to [$VERSION]"
-    echo ""
-
-    for DIRECTORY in $DIRECTORIES
-    do
-        cd $DIRECTORY
-        systemCall "./grailsw set-version $VERSION"
-        systemCall "git add ."
-        git commit -m"synchronizing version"
-        systemCall "git push origin master"
-        cd -
-    done
-
-}
 
 if git diff-index --quiet HEAD --; then
     echo "everything is up to date, safe to synchronize versions and release"
@@ -47,15 +16,6 @@ else
     exit 1
 fi
 
-synchronizeVersions
-
-echo ""
-echo "Releasing ${VERSION} to GitHub"
-echo ""
-
-systemCall "git tag -a v${VERSION} -m 'tagging release'"
-systemCall "git push origin v${VERSION}"
-
 #release to bintray
 echo ""
 echo "Releasing ${VERSION} to BinTray"
@@ -63,6 +23,11 @@ echo ""
 
 echo "resolving metridoc-core first"
 cd metridoc-grails-core
+
+systemCall "./grailsw set-version $VERSION"
+systemCall "git add ."
+git commit -m"synchronizing version"
+systemCall "git push origin master"
 systemCall "./grailsw --refresh-dependencies --non-interactive tA --stacktrace"
 systemCall "./grailsw --refresh-dependencies --non-interactive upload-to-bintray --failOnBadCondition=false"
 cd -
@@ -72,6 +37,10 @@ for DIRECTORY in $DIRECTORIES
     if [ "$DIRECTORY" != "./metridoc-grails-core" ]; then
         echo "uploading [$DIRECTORY]"
         cd $DIRECTORY
+        systemCall "./grailsw set-version $VERSION"
+        systemCall "git add ."
+        git commit -m"synchronizing version"
+        systemCall "git push origin master"
         systemCall "./grailsw --refresh-dependencies --non-interactive tA --stacktrace"
         systemCall "./grailsw --refresh-dependencies --non-interactive upload-to-bintray --failOnBadCondition=false"
         cd -
@@ -81,6 +50,13 @@ done
 MINOR_VERSION=`sed 's/[0-9]*\.[0-9]*\.\([0-9]*\).*/\1/g' VERSION`
 MAJOR_VERSION=`sed 's/\([0-9]*\.[0-9]*\).*/\1/g' VERSION`
 NEW_VERSION="$MAJOR_VERSION.$((MINOR_VERSION+1))-SNAPSHOT"
+
+echo ""
+echo "Releasing ${VERSION} to GitHub"
+echo ""
+
+systemCall "git tag -a v${VERSION} -m 'tagging release'"
+systemCall "git push origin v${VERSION}"
 
 echo ""
 echo "Bumping version to [$NEW_VERSION]"
