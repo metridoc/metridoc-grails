70162ae97235aaae963f07a802e2ba0d51f97ffb
tbarker <mingus.karate@gmail.com>
2013-11-26 10:26:24 -0500

    synchronizing version

---
+++ bumpVersion.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+MINOR_VERSION=`sed 's/[0-9]*\.[0-9]*\.\([0-9]*\).*/\1/g' VERSION`
+MAJOR_VERSION=`sed 's/\([0-9]*\.[0-9]*\).*/\1/g' VERSION`
+NEW_VERSION="$MAJOR_VERSION.$((MINOR_VERSION+1))-SNAPSHOT"
+
+echo ""
+echo "Releasing ${VERSION} to GitHub"
+echo ""
+
+systemCall "git tag -a v${VERSION} -m 'tagging release'"
+systemCall "git push origin v${VERSION}"
+
+echo ""
+echo "Bumping version to [$NEW_VERSION]"
+echo ""
+
+echo "$NEW_VERSION" > VERSION
+
+systemCall "git add VERSION"
+systemCall "git commit -m 'committing a new version'"
+systemCall "git push origin master"
+
+echo "resolving metridoc-core first"
+cd metridoc-grails-core
+
+systemCall "./grailsw set-version $VERSION"
+systemCall "git add ."
+git commit -m"synchronizing version"
+systemCall "git push origin master"
+cd -
+
+for DIRECTORY in $DIRECTORIES
+    do
+    if [ "$DIRECTORY" != "./metridoc-grails-core" ]; then
+        echo "uploading [$DIRECTORY]"
+        cd $DIRECTORY
+        systemCall "./grailsw set-version $VERSION"
+        systemCall "git add ."
+        git commit -m"synchronizing version"
+        systemCall "git push origin master"
+        cd -
+    fi
+done
+
+
