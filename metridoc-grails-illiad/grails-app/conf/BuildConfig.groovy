26269330d79c95dd43a55ddff113ecee112ab9ff
tbarker <mingus.karate@gmail.com>
2014-01-12 12:21:55 -0500

    upgrade illiad to 2.3.4
    upgraded illiad to 2.3.4, but had to add spring-test directly via
    BuildConfig.  Not really sure why this was required.  Also upgraded
    the wrapper.

--- metridoc-grails-illiad/grails-app/conf/BuildConfig.groovy
+++ metridoc-grails-illiad/grails-app/conf/BuildConfig.groovy
@@ -24,7 +24,7 @@ grails.project.repos.default = "metridocRepo"
 File versionFile = new File(new File(basedir).parent, "VERSION")
 
 String coreVersion
-boolean useInlinePlugin
+boolean useInlinePlugin = false
 if (versionFile.exists()) {
     coreVersion = versionFile.getText("utf-8").trim()
     useInlinePlugin = coreVersion.endsWith("SNAPSHOT")
@@ -45,17 +45,41 @@ grails.project.dependency.resolution = {
         legacyResolve true
     }
 
+    grails.project.fork = [
+            // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+            //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+            // configure settings for the test-app JVM, uses the daemon by default
+            test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+            // configure settings for the run-app JVM
+            run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+            // configure settings for the run-war JVM
+            war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+            // configure settings for the Console UI JVM
+            console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+    ]
+
+    grails.project.dependency.resolver = "maven"
+
     inherits("global")
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
+        inherits true // Whether to inherit repository definitions from plugins
+        mavenLocal()
+        grailsPlugins()
+        grailsHome()
         grailsCentral()
         mavenCentral()
+        mavenRepo "https://oss.sonatype.org/content/repositories/snapshots"
         mavenRepo "http://dl.bintray.com/upennlib/metridoc"
         mavenRepo "http://dl.bintray.com/upennlib/maven"
+        mavenRepo "http://jcenter.bintray.com/"
     }
 
     dependencies {
         compile 'net.sf.opencsv:opencsv:2.3'
+        //not sure why I need this, but the resolution step could not find it
+        test 'org.springframework:spring-test:3.2.5.RELEASE'
     }
 
     plugins {
