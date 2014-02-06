47ae25d8068563d958e3ffa6f7aa7aaffa1eefb0
tbarker <mingus.karate@gmail.com>
2014-02-06 17:57:21 -0500

    bump version

--- metridoc-grails-illiad/MetridocIlliadGrailsPlugin.groovy
+++ metridoc-grails-illiad/MetridocIlliadGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class MetridocIlliadGrailsPlugin {
-    def version = "0.7.12-SNAPSHOT"
+    def version = "0.7.12"
     def grailsVersion = "2.0 > *"
 
     def title = "Metridoc Illiad Plugin" // Headline display name of the plugin
