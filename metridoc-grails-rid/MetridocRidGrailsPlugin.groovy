99c04844a2b63f722c2651360d8f5f4420886ac7
jenkins <jenkins@jenkins.library.upenn.edu>
2014-03-07 15:26:19 -0500

    synchronizing version

--- metridoc-grails-rid/MetridocRidGrailsPlugin.groovy
+++ metridoc-grails-rid/MetridocRidGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class MetridocRidGrailsPlugin {
-    def version = "0.7.12-SNAPSHOT"
+    def version = "0.7.12"
     def grailsVersion = "2.1 > *"
     def loadAfter = ['metridocCore']
     def pluginExcludes = [
