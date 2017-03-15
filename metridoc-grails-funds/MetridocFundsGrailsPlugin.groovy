522cfa244bea04c05cfc2ec1f913d6ae26ab685a
jenkins <jenkins@jenkins.library.upenn.edu>
2017-03-15 12:32:07 -0400

    synchronizing version

--- metridoc-grails-funds/MetridocFundsGrailsPlugin.groovy
+++ metridoc-grails-funds/MetridocFundsGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class MetridocFundsGrailsPlugin {
-    def version = "0.8.6"
+    def version = "0.8.9"
     def grailsVersion = "2.0 > *"
 
     def title = "Metridoc Illiad Plugin" // Headline display name of the plugin
