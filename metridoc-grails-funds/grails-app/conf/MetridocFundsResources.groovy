f870e7c5afd53a1d8adb2033c5c011f57911e0dd
Gavin <gavinyoung1994@gmail.com>
2017-05-16 11:13:55 -0400

    deleted metridoc-grails-funds folder

--- metridoc-grails-funds/grails-app/conf/MetridocFundsResources.groovy
+++
@@ -1,7 +0,0 @@
-modules = {
-    funds {
-        resource id: 'css',
-                url: [plugin: 'metridocFunds', dir: 'css', file: 'funds_main.css'],
-                attrs: [type: 'css']
-    }
-}
