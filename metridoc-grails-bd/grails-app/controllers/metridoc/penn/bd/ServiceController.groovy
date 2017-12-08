2c66c36704d4181a1369fe4271fb932dfca4a2f8
Gavin <gavinyoung1994@gmail.com>
2017-12-08 15:09:26 -0500

    almost completely formatted

--- metridoc-grails-bd/grails-app/controllers/metridoc/penn/bd/ServiceController.groovy
+++ metridoc-grails-bd/grails-app/controllers/metridoc/penn/bd/ServiceController.groovy
@@ -28,8 +28,8 @@ class ServiceController {
 		if(dateFrom != null && dateTo != null && library_id != null){
 			def library = borrowDirectService.getLibraryById(params.serviceKey, library_id)
 			def libname = library != null?library.institution:""
-			response.setHeader("Content-Disposition", "attachment;filename=\""+libname+"_data_dump_"+params.dateFrom+"-"+params.dateTo+".xlsx\"");
-			response.setContentType("application/vnd.ms-excel")
+			response.setHeader("Content-Disposition", "attachment;filename=\""+libname+"_data_dump_"+params.dateFrom+"-"+params.dateTo+".csv\"");
+			response.setContentType("text/csv")
 			//we added 1 since dateTo without time will convert to 12am of the last day, thus skipping all data for
             //the last day
             borrowDirectService.dumpDataLibrary(library_id, dateFrom, dateTo + 1, response.outputStream, params.serviceKey)
